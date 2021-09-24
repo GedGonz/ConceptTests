a78b41d05c642d09df308367ef61c0a5f74e72f2
Gerald Gonzalez <gedgonz7@gmail.com>
2021-09-23 21:00:46 -0600

    interview practice on counting words in an array

---
+++ TestIneterview/CountWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIneterview
+{
+    public class CountWords
+    {
+        public Dictionary<string, int> getCountWords() {
+
+            var dictornatyCountWord = new Dictionary<string, int>();
+            var words =new [] { "javascript","C#","C++","JAVA","Ruby", "javascript", "javascript","JAVA","C#" };
+
+            for (var i=0; i<words.Length; i++) {
+
+                if (dictornatyCountWord.ContainsKey(words[i]))
+                {
+                    dictornatyCountWord.TryGetValue(words[i], out var countWordfinded);
+                    dictornatyCountWord[words[i]] = ++countWordfinded;
+                }
+                else
+                    dictornatyCountWord.Add(words[i], 1);
+            }
+            return dictornatyCountWord;
+        }
+    }
+}
---
+++ UnitTests/AcountWordsShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIneterview;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitTests
+{
+    public class AcountWordsShould
+    {
+        [Fact]
+        public void CountWords() {
+
+            //Arange
+            var dictornatyCountWord = new Dictionary<string, int>();
+            dictornatyCountWord.Add("javascript",3);
+            dictornatyCountWord.Add("C#", 2);
+            dictornatyCountWord.Add("C++", 1);
+            dictornatyCountWord.Add("JAVA", 2);
+            dictornatyCountWord.Add("Ruby", 1);
+            //Act
+            var countWords = new CountWords();
+
+            var dictornaryCount = countWords.getCountWords();
+
+            dictornaryCount.Should().BeEquivalentTo(dictornatyCountWord);
+        }
+    }
+}
