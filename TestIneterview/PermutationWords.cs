a10d9b3ef3d06a223a44345ad998846fc2532052
Gerald Gonzalez <gedgonz7@gmail.com>
2021-09-28 11:58:40 -0600

    creation of tests for the verification of permutation of equal words

---
+++ TestIneterview/PermutationWords.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIneterview
+{
+    public class PermutationWords
+    {
+        public (Dictionary<string,int>, Dictionary<string, int>, bool) getPermutation(string word1, string word2) {
+
+            var dictionaryWord1 = new Dictionary<string, int>();
+            var dictionaryWord2 = new Dictionary<string, int>();
+
+            if (word1.Length != word2.Length)
+                return (dictionaryWord1, dictionaryWord2, false);
+
+            var charactersWord1 = word1.ToArray();
+            var charactersWord2 = word2.ToArray();
+
+            dictionaryWord1 = getCountCharacters(charactersWord1);
+            dictionaryWord2 = getCountCharacters(charactersWord2);
+
+            return (dictionaryWord1, dictionaryWord2, true);
+        }
+
+        public Dictionary<string, int> getCountCharacters(char[] characters)
+        {
+
+            var dictornatyCountCharacter = new Dictionary<string, int>();
+
+            for (var i = 0; i < characters.Length; i++)
+            {
+
+                if (dictornatyCountCharacter.ContainsKey(characters[i].ToString()))
+                {
+                    dictornatyCountCharacter.TryGetValue(characters[i].ToString(), out var countWordfinded);
+                    dictornatyCountCharacter[characters[i].ToString()] = ++countWordfinded;
+                }
+                else
+                    dictornatyCountCharacter.Add(characters[i].ToString(), 1);
+            }
+            return dictornatyCountCharacter;
+        }
+
+
+    }
+}
---
+++ UnitTests/PermutationWordsShould.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIneterview;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitTests
+{
+    public class PermutationWordsShould
+    {
+        [Fact]
+        public void VerifyPermutatioWordsIsCorrect() {
+
+            //Arange
+            const string word1 = "CASA";
+            const string word2 = "SACA";
+            var dictornatyPermutationWord = new Dictionary<string, int>
+            {
+                { "C", 1 },
+                { "A", 2 },
+                { "S", 1 }
+            };
+
+            //Act
+            var permutationsWords = new PermutationWords();
+
+            var (dictornaryWord1, dictornaryWord2, _) = permutationsWords.getPermutation(word1, word2);
+
+            //Asert
+            dictornaryWord1.Should().BeEquivalentTo(dictornatyPermutationWord);
+            dictornaryWord2.Should().BeEquivalentTo(dictornatyPermutationWord);
+        }
+
+        [Fact]
+        public void VerifyDifferentWord()
+        {
+            //Arange
+            const string word1 = "CAMISA";
+            const string word2 = "SACA";
+
+            //Act
+            var permutationsWords = new PermutationWords();
+
+            var (_, _, state) = permutationsWords.getPermutation(word1, word2);
+
+            //Asert
+            state.Should().BeFalse();
+        }
+        [Fact]
+        public void VerifyLengthWordEqualsButDiferentCountLetters()
+        {
+
+            //Arange
+            const string word1 = "CASA";
+            const string word2 = "CACA";
+            var dictornatyPermutationWord = new Dictionary<string, int>
+            {
+                { "C", 1 },
+                { "A", 2 },
+                { "S", 1 }
+            };
+
+            //Act
+            var permutationsWords = new PermutationWords();
+
+            var (_, dictornaryWord2, _) = permutationsWords.getPermutation(word1, word2);
+
+            //Asert
+            dictornaryWord2.Should().NotEqual(dictornatyPermutationWord);
+        }
+    }
+}
