1dca5da765b4ef483ece97df3e7e974276d9d001
Gerald Gonzalez <gedgonz7@gmail.com>
2021-10-06 22:29:08 -0600

    practice with algorithmic binary search

---
+++ TestIneterview/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIneterview
+{
+    public class BinarySearch
+    {
+        public int Search(int []array, int findNumber) {
+
+            var min = 0;
+            var max = array.Length;
+            
+            while (max>0) {
+
+                var mid = (min + max) / 2;     //tomo el indice de en medio del array
+
+                if (array[mid] == findNumber)  //verifico que si se encuentra en la posicion calculada
+                    return mid;
+                if (array[mid] < findNumber)   //verifico que si el valor esta a la izquierda o derecha
+                    min = mid + 1;
+                else
+                    max = mid - 1;
+            }
+            return -1;                        //En caso de no encontrar el valor se retorna -1
+        }
+    }
+}
---
+++ UnitTests/BinarySearchShould.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIneterview;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitTests
+{
+    public class BinarySearchShould
+    {
+        [Fact]
+        public void VerifySearchBinaryIsCorrect() {
+
+            //Arange
+            int findNumber=11;
+            int []array = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+
+            //Act
+            var binarysearch = new BinarySearch();
+
+            var index = binarysearch.Search(array, findNumber);
+
+            //Asert
+            index.Should().Be(4);
+        }
+
+        [Fact]
+        public void VerifySearchBinaryNotFoundResult()
+        {
+
+            //Arange
+            int findNumber = 1;
+            int[] array = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+
+            //Act
+            var binarysearch = new BinarySearch();
+
+            var index = binarysearch.Search(array, findNumber);
+
+            //Asert
+            index.Should().Be(-1);
+        }
+
+    }
+}
